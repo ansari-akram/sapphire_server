97db742c45f00ee58c67be9a4e3a9cf8ce0197b5
ansari-akram <ansariakramobaid@gmail.com>
2022-06-01 18:44:54 +0530

    settings changes

--- sapphire_chatbot/urls.py
+++ sapphire_chatbot/urls.py
@@ -19,9 +19,14 @@ from chatbot_appv1.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('response/', get_response),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
