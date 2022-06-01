6413f789e660532006c0f687485c8480a369bb92
ansari-akram <ansariakramobaid@gmail.com>
2022-06-01 18:26:16 +0530

    settings changes

--- sapphire_chatbot/settings.py
+++ sapphire_chatbot/settings.py
@@ -132,3 +132,13 @@ MEDIA_URL = '/chatbot_appv1/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'chatbot_appv1/media/')  
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
