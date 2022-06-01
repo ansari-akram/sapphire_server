f62e78b5f05c7b223682c05b5829dcb45a7fbf59
ansari-akram <ansariakramobaid@gmail.com>
2022-06-01 20:10:46 +0530

    settings changes

--- sapphire_chatbot/settings.py
+++ sapphire_chatbot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sido#3j@v4=_)yznbeo34c=kcmd1jt2&w92eq-tqea74uxop)!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
